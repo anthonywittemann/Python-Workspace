f71c14eb6f71e9646a07059f4a2e4bf81fd02df8
anthonywittemann <awittema@usc.edu>
2015-05-28 00:04:34 -0700

    functions and conditionals

--- Python 3 Essential Training/Working/syntax.py
+++ Python 3 Essential Training/Working/syntax.py
@@ -15,9 +15,32 @@ def main():
     print(a)
     a = [1, 2, 3, 4, 5] # list
     print(a)
+    
+    # conditionals
+    a, b = 0, 1
+    if a < b:
+        print("a < b")
+    elif a > b:
+        print("a > b") 
+    else:
+        print("a == b")
+    
+    # defining and using functions
     egg() # would not work if there were no main function
+    func(3)
+    func(12) # doesn't work
+    func()
 
 def egg():
     print("egg")
+    
+def func(a = 0): # default value to be assigned to a
+    for i in range(a, 10):
+        print(i, end=' ')
+
+
+x, y = 0, 1
+s = "<" if x < y else ">=" # conditional expression
+print(s)
 
 if __name__ == "__main__": main()
